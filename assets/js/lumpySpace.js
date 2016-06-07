2cc9ca1f5c9f0b250236d2ce323c8c94608f4f37
Jordan Long <jordan.long222@gmail.com>
2016-06-07 13:59:48 -0400

    First lo_dash test that works

--- assets/js/lumpySpace.js
+++ assets/js/lumpySpace.js
@@ -1,3 +1,11 @@
 $(function(){
   'use strict';
 });
+
+var x = 1;
+console.log(x);
+
+
+var greeting = _.template(', <%- m.name %>!', { variable: 'm' });
+var renderedHtml = greeting({ name: '' });
+$('#lo_name').html(renderedHtml);
