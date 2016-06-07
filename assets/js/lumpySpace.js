bb7300e0a9ec5eade66a2e9982ddcf1c2377be2f
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-07 17:16:24 -0400

    stuff and things

--- assets/js/lumpySpace.js
+++ assets/js/lumpySpace.js
@@ -2,24 +2,28 @@ $(function(){
   'use strict';
 });
 
+var x = 1;
+console.log(x);
 
 
-$(function(){
-  $('topSearch').on('submit', function(e){
-  e.preventDefault();
 
-  var ghSidebar = $('input [class = "searchBox"]').val();
+$(function () {
+ $('#topSearch').on('submit', function(e) {
+   e.preventDefault();
 
-  $.getJSON('https://api.github.com/users' + ghSidebar)
-    .done(showUser);
-});
+   var ghSidebar = $('input[class="searchBox"]').val();
 
-function show(template, model) {
-   var fn = _.template($('#' + template).html(), { variable: 'm' });
-   $('.user-info').html(fn(model));
+   $.getJSON('https://api.github.com/users/' + ghSidebar)
+     .done(showUser);
+ });
+
+ function showUser(user) {
+   show('gh-user-template', user);
  }
 
-});
-
-
+ function show (template, model) {
+   var fn = _.template($('#' + template).html(), {
+     variable: 'm'});
+   $('.SBuser_info').html(fn(model));
+   }
 });
