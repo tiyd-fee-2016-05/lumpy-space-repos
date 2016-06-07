6b766e90bba118de4a421e54ed3d7d4150b18a30
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-07 16:01:48 -0400

    made changes

--- assets/js/lumpySpace.js
+++ assets/js/lumpySpace.js
@@ -1,3 +1,25 @@
 $(function(){
   'use strict';
 });
+
+
+
+$(function(){
+  $('topSearch').on('submit', function(e){
+  e.preventDefault();
+
+  var ghSidebar = $('input [class = "searchBox"]').val();
+
+  $.getJSON('https://api.github.com/users' + ghSidebar)
+    .done(showUser);
+});
+
+function show(template, model) {
+   var fn = _.template($('#' + template).html(), { variable: 'm' });
+   $('.user-info').html(fn(model));
+ }
+
+});
+
+
+});
