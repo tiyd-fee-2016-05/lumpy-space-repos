7bba31890bc020d300f08e6d224a0061559f0742
Jordan Long <jordan.long222@gmail.com>
2016-06-08 15:02:04 -0400

    Add repo object array w/ ajax call

--- assets/js/lumpySpace.js
+++ assets/js/lumpySpace.js
@@ -5,20 +5,28 @@ $(function(){
 var x = 1;
 console.log(x);
 
+// INPUT FUNCTION ASSIGNMENT
 $(function () {
   $('#topSearch').on('submit', function(e) {
     e.preventDefault();
 
     var ghSidebar = $('input[class="searchBox"]').val();
 
+// USER API AJAX CALL
     $.getJSON('https://api.github.com/users/' + ghSidebar)
       .done(showUser)
       .fail(showError);
+
+// REPO API AJAX CALL
+    $.getJSON('https://api.github.com/users/' + ghSidebar + '/repos')
+      .done(showRepo);
   });
+
 //gh-user-template function
   function showUser(user) {
     show('gh-user-template', user);
     showA('gh-avatar-template', user);  //Instead of adding an entire new showAvatar function. You can add combine them here.
+
   }
 //ERROR MESSAGE
   function showError(req, status, err) {
@@ -31,7 +39,7 @@ $(function () {
   function show (template, model) {
     var fn = _.template($('#' + template).html(), {
       variable: 'm'});
-    $('.SBuser_info').html(fn(model));   //assigning location if successful 
+    $('.SBuser_info').html(fn(model));   //assigning location if successful
     }
 
 // gh-avatar-template
@@ -45,6 +53,21 @@ $(function () {
     var fn = _.template($('#' + template).html(), {
       variable: 'm'});
     $('.NBuser_info').html(fn(model));   //corresponds with the empty DIV location upon input
+    }
+
+//SHOW REPOSITORIES
+  function showRepo(repo) {               //this API looks like it is in an array - for loop needed to capture each element
+    for (r=0; r<showRepo.length; r++){    //
+      showR('gh-repo-template', repo[r]); //print what is found when the for loop block of code is run.
+      console.log(repo[r]);
+    //this prints the object and it's array. if we're able to assign the object a variable, we can pull out the object information with obj.name
+    }
+  }
+
+  function showR (template, model) {
+    var fn = _.template($('#' + template).html(), {
+      variable: 'r'});
+    $('.repo_info').html(fn(model));
     }
 
   });
